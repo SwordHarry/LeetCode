1c11491c7aac03f2195886bd794759ef1e47c22b
一把健 <2398960068@qq.com>
2019-02-04 20:30:03 +0800

    算法打卡第十四天-熟悉二叉树遍历

--- 初始算法/树/二叉树/深度遍历_迭代/后序遍历_迭代.js
+++ 初始算法/树/二叉树/深度遍历_迭代/后序遍历_迭代.js
@@ -9,30 +9,37 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var postorderTraversal = function (root) {
-    const nums = [];
-    if (!root) {
-        return nums;
-    }
 
-    const stack = [];
-    let node = root;
-    let top, visitNode = null;
-
-    while (node || stack.length) {
-        if (node) {
-            stack.push(node);
-            node = node.left
-        } else {
-            top = stack[stack.length - 1];
-            if (top.right && visitNode !== top.right) {
-                node = top.right;
-            } else {
-                visitNode = stack.pop();
-                nums.push(visitNode.val);
-            }
-        }
-    }
-
+/**
+ * 主要思想：首先遍历root根节点的所有左节点，并依次入栈。
+ *         对出栈的元素，如果没有右儿子或者虽然有右儿子但右儿子已完成遍历，即可完成出栈；
+ *         否则，再次入栈，并把右儿子入栈，遍历右儿子的所有左儿子。
+ */
+var postorderTraversal = function(root) {
+  const nums = [];
+  if (!root) {
     return nums;
+  }
+
+  const stack = [];
+  let node = root;
+  let top,
+    visitNode = null;
+
+  while (node || stack.length) {
+    if (node) {
+      stack.push(node);
+      node = node.left;
+    } else {
+      top = stack[stack.length - 1];
+      if (!top.right || visitNode === top.right) {
+        visitNode = stack.pop();
+        nums.push(visitNode.val);
+      } else {
+        node = top.right;
+      }
+    }
+  }
+
+  return nums;
 };
