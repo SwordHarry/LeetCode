aa0857bd3a6562d6cd19b60971f7929465d208cb
一把健 <2398960068@qq.com>
2019-02-28 15:17:26 +0800

    算法打卡第三十九天

---
+++ 内部排序/72.归并排序.js
@@ -0,0 +1,35 @@
+/**
+ * 熟悉高级排序算法之：归并排序
+ *  2-路归并排序的核心操作 是 将一维数组中前后相邻的两个有序序列归并为一个有序序列
+ */
+function MSort(arr) {
+    const len = arr.length;
+    if (len > 1) {
+        const mid = ~~(len / 2);
+        const left = arr.slice(0, mid);
+        const right = arr.slice(mid);
+        MSort(left);
+        MSort(right);
+        return _merge(MSort(left), MSort(right));
+    } else {
+        return arr;
+    }
+}
+
+function _merge(left, right) {
+    const arr = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+
+    return arr.concat(left, right);
+}
+
+const arr = [49, 38, 65, 97, 76, 13, 27];
+const newArr = MSort(arr);
+
+console.log(newArr, arr);
