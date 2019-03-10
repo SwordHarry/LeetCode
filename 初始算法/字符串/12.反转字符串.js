530eeaa4e97992e4f2858141206677fc5783a428
一把健 <2398960068@qq.com>
2019-03-10 13:04:58 +0800

    算法打卡第四十七天

--- 初始算法/字符串/12.反转字符串.js
+++ 初始算法/字符串/12.反转字符串.js
@@ -2,16 +2,10 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {
-    var len = s.length - 1,
-        halfLen = ~~(s.length / 2),
-        i,temp;
-    for(i = 0;i < halfLen;i++) {
-        temp = s[i];
-        s[i] = s[len - i];
-        s[len - i] = temp;
-    }
+var reverseString = function (s) {
+    const len = s.length;
 
-    // or
-    // s.reverse();
+    for (let i = 0; i < ~~(len / 2); i++) {
+        [s[i], s[len - i - 1]] = [s[len - i - 1], s[i]];
+    }
 };
